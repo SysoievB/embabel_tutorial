6ab6592673c1f87ae439b196885e4f3a571691dd
SysoievB <sysoiev.bogdan.ua@gmail.com>
2026-04-17 15:10:37 +0200

    Initial commit

---
+++ src/main/java/com/embabel_tutorial/BlogDraft.java
@@ -0,0 +1,4 @@
+package com.embabel_tutorial;
+
+public record BlogDraft() {
+}
---
+++ src/main/java/com/embabel_tutorial/EmbabelTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.embabel_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbabelTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbabelTutorialApplication.class, args);
+    }
+
+}
