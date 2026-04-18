a04287d8f4daafec8b91630ae016cd87d37dbaee
SysoievB <sysoiev.bogdan.ua@gmail.com>
2026-04-18 07:56:53 +0200

    Added DTOs

---
+++ src/main/java/com/embabel_tutorial/BlogWriterAgent.java
@@ -0,0 +1,11 @@
+package com.embabel_tutorial;
+
+import com.embabel.agent.api.annotation.Agent;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Agent(description = "Write and review a blog post about a given topic")
+public class BlogWriterAgent {
+
+
+}
---
+++ src/main/java/com/embabel_tutorial/ReviewedPost.java
@@ -0,0 +1,4 @@
+package com.embabel_tutorial;
+
+public record ReviewedPost(String title, String content, String feedback) {
+}
